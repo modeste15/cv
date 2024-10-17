a2b34b74c2812b29042f24f49c1e23eb185917d1
modeste kouassi <modeste.kouassi@workace.fr>
2024-10-17 16:25:50 +0200

    French

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -17,6 +17,9 @@ import {
   TastyCloudLogo,
   YearProgressLogo,
 } from "@/images/logos";
+
+
+
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
@@ -25,9 +28,9 @@ export const RESUME_DATA = {
   location: "Angers, France, & 🌏",
   locationLink: "https://maps.app.goo.gl/gjmCCWuzWxo4TF8J6",
   about:
-    "Software Engineer, Data Enthusiast.",
+    "Developpement Informatique et Manipulation de Données ",
   summary:
-    "I have a passion for delivering solutions to various industries, including commerce, insurance, and project management, through the use of technological solutions. I apply my skills to the creation of advanced software solutions. My expertise spans from design to implementation, with a particular focus on solving challenges that businesses may encounter in their daily tasks.",
+    "Passionné par le développement logiciel, je suis actuellement Chargé de développement en alternance chez Malakoff Humanis, où je reçois les besoins des équipes commerciales et les implémente dans un logiciel dédié au suivi d’activité. En parallèle, je mène de nombreux projets en ingénierie data et analyse de données . Fort d’une expérience variée, j’ai occupé un poste  de développeur chez Orange France, où j’ai mené des projets d’intégration d’API et automatisé des processus clés, tels que la signature électronique. Je maîtrise les bonnes pratiques de développement et suis habitué à travailler en méthodologie agile. Mon expertise couvre une variété de technologies, allant de la gestion des bases de données SQL à l’intégration de solutions web complexes ainsi que la gestions de projet Informatique.",
   avatarUrl: "https://avatars.githubusercontent.com/u/49912886?s=400&u=b3e9e8682bcceb42ccc332b97cca075fb8036947&v=4",
   personalWebsiteUrl: "https://medouse.me",
   contact: {
@@ -43,30 +46,25 @@ export const RESUME_DATA = {
         name: "LinkedIn",
         url: "https://www.linkedin.com/in/modestekouassi/",
         icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://twitter.com/medouse_",
-        icon: XIcon,
-      },
+      }
     ],
   },
   education: [
     {
       school: "SUPINFO",
-      degree: "Master's in Data Engineering",
+      degree: "Master Data Engineering",
       start: "2023",
       end: "2025",
     },
     {
       school: "Université du Havre Normandie",
-      degree: "Bachelor's degree in Computer Science",
+      degree: "Licence Informatique ",
       start: "2021",
       end: "2022",
     },
     {
       school: "Ecole Polytechnique  Lome",
-      degree: "Professional Bachelor's Program in Software Engineering",
+      degree: "Parcours Licence Professionnelle Genie Logiciel ",
       start: "2017",
       end: "2020",
     }
@@ -75,35 +73,34 @@ export const RESUME_DATA = {
     {
       company: "Malakoff Humanis",
       link: "https://www.malakoffhumanis.com/",
-      badges: ["Apprenticeship"],
-      title: "Consultant Software ",
+      badges: ["Alternance"],
+      title: "Chargé de developpement Informatique ",
       logo: ParabolLogo,
       start: "2023",
       end: "2025",
       description:
-        "In my role as a development manager for a web-based software dedicated to activity tracking within a CRM for the sales field, I oversee the continuous improvement of the system and generate analytical reports through SQL data manipulation.",
+        "Dans mon rôle de chargé de développement pour un logiciel web dédié au suivi d’activité au sein d’un CRM orienté vers le domaine commercial, je recueille les besoins métiers et mets en œuvre des solutions pour améliorer continuellement le système. J’élabore également des rapports analytiques en effectuant des manipulations avancées des données via SQL, afin de soutenir la prise de décision et optimiser la performance.",
     },
     {
       company: "Orange France",
       link: "https://www.orange.fr",
-      badges: ["Hybrid"],
-      title: "Laravel PHP/Developer",
+      title: "Developpeur Laravel ",
       logo: ClevertechLogo,
       start: "2022",
       end: "2023",
       description:
-      "As a Lead Developer, I was tasked with maintaining and evolving a project management software. I integrated various automation APIs and successfully led integrations, such as Electronic Signature functionality." ,
+      "En tant qu’Analyste Développeur, j’étais chargé de maintenir et de faire évoluer un logiciel de gestion de projet. J’ai intégré diverses API d’automatisation et mené avec succès des intégrations, telles que la fonctionnalité de signature électronique ou une intégration des API de l’INSEE. J’ai également travaillé sur du DevOps et utilisé des outils d’analyse tels que SonarQube." ,
     },
     {
       company: "Engenys",
       link: "https://engenys.com/",
-      badges: ["Internship"],
+      badges: ["Stage"],
       title: "Software Engineer",
       logo: JojoMobileLogo,
       start: "2022",
       end: "2022",
       description:
-      "Evolutionary maintenance of a Python web-based CMMS (Computerized Maintenance Management System) application using the Flask micro-framework, along with Jinja and Tailwind CSS technologies, and Docker integration.",
+      "Maintenance évolutive d’une application GMAO (Gestion de Maintenance Assistée par Ordinateur) web basée sur Python, utilisant le micro-framework Flask, ainsi que les technologies Jinja et Tailwind CSS, et l’intégration de Docker.",
     },
     {
       company: "Clinicaa",
@@ -113,18 +110,9 @@ export const RESUME_DATA = {
       logo: NSNLogo,
       start: "2019",
       end: "2021",
-      description: "Developer position within a team dedicated to creating a comprehensive hospital solution, developed entirely from scratch. My role involved designing and coding crucial features, contributing to the construction of a robust system tailored to the specific needs of the healthcare sector. My commitment to this project enhanced my development skills and deepened my understanding of the complex requirements of medical solutions.",
+      description: "Poste de développeur au sein d’une équipe dédiée à la création d’une solution hospitalière complète, développée entièrement à partir de zéro. Mon rôle consistait à concevoir et coder des fonctionnalités cruciales, contribuant à la construction d’un système robuste, adapté aux besoins spécifiques du secteur de la santé. Mon engagement dans ce projet a renforcé mes compétences en développement et approfondi ma compréhension des exigences complexes des solutions médicales.",
     },
-    {
-      company: "Miabe Star",
-      link: "",
-      badges: [],
-      title: "Junior Laravel Developper",
-      logo: NSNLogo,
-      start: "2019",
-      end: "2019",
-      description: "Design and development of backend APIs for a mobile application.",
-    },
+
   ],
   skills: [
     "JavaScript",
@@ -137,6 +125,51 @@ export const RESUME_DATA = {
 
   ],
   projects: [
+    {
+      title: "API pour Gestion de Bibliothèque",
+      techStack: [
+        "School Project",
+        "FASTAPI",
+        "ORACLE",
+        "GITHUB WORKFLOWS"
+      ],
+      description: "Api avec FastApi et une base de données Oracle",
+      logo: ConsultlyLogo,
+      link: {
+        label: "",
+        href: ""
+      },
+    }, 
+    {
+      title: "Streaming with Kafka",
+      techStack: [
+        "Kafka ",
+        "Python",
+        "PySpark",
+        "ML",
+        "Postgres"
+      ],
+      description: "Récupération API mastodon en temps réeelle (Chaque 5s) pour l'analyse de sentiments des postes",
+      logo: ConsultlyLogo,
+      link: {
+        label: "Présentation ICI",
+        href: "https://modeste.my.canva.site/pyspark"
+      },
+    }, 
+    {
+      title: "DATAVIZ with Tableau",
+      techStack: [
+        "School Project",
+        "Tableau",
+        "CSV",
+      ],
+      description: "Visualisation des données sur la recherche scientifique",
+      logo: ConsultlyLogo,
+      link: {
+        label: "",
+        href: ""
+      },
+    }, 
     {
       title: "Wenkel",
       techStack: [
